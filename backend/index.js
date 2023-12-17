58870b64e055a2e9598dca75c7792ba65d34071b
Prasoon Kumar <76505015+Prasoon2050@users.noreply.github.com>
2023-12-17 19:51:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/signupapp";
+  process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
